b949824525bee5ca4ad9f7afffd755aa6ff7ce53
Eugeny Loy <eugeny.loy@gmail.com>
2014-10-15 23:32:11 +0300

    Minor cleanup

--- src/main/scala/org/bidpulse/example/DummyPublisher.scala
+++ src/main/scala/org/bidpulse/example/DummyPublisher.scala
@@ -6,17 +6,11 @@ import org.bidpulse.pipeline.{Channel, Pipeline, Publisher}
 
 class DummyPublisher(pipeline: ActorRef) extends Actor with Publisher with ActorLogging {
 
-
   pipeline ! Pipeline.Subscribe
 
-
   override def receive: Receive = {
-    case msg: Channel.ProjectsPublished =>
-      log.info(s"message published $msg")
-    case msg: Channel.ProjectPublished =>
-      log.info(s"message published $msg")
-    case msg: Channel.ProjectUpdatePublished =>
-      log.info(s"message published $msg")
+    case msg @ (_: Channel.ProjectsPublished | _: Channel.ProjectPublished | _: Channel.ProjectUpdatePublished) =>
+      log.info(s"Message published\n>>>> $msg")
   }
 
 }
--- src/main/scala/org/bidpulse/example/DummySource.scala
+++ src/main/scala/org/bidpulse/example/DummySource.scala
@@ -1,5 +1,7 @@
 package org.bidpulse.example
 
+import java.util.UUID
+
 import akka.actor.{ActorLogging, Actor, ActorRef, Cancellable}
 import org.bidpulse.domain.{FreelancerId, Project, Unread}
 import org.bidpulse.pipeline.Source.Init
@@ -9,28 +11,41 @@ import scala.concurrent.duration._
 
 class DummySource extends Actor with Source with ActorLogging {
 
-  var channel: ActorRef = null
-  var subscription: Cancellable = null
+  import context.dispatcher
 
-  implicit val global = scala.concurrent.ExecutionContext.Implicits.global
+  var channel: ActorRef = null
+  var subscription: Option[Cancellable] = None
 
   override def receive: Receive = {
     case Init(ch) =>
+      // initialise - subscribe to channel
       channel = ch
       channel ! Channel.Subscribe(self, false)
     case Channel.ProjectsPublished(_) =>
-      val msg = Channel.PublishProject(Project(FreelancerId("id"), null, Unread, None, None, None, None, None, None, None, None, None))
-      subscription = context.system.scheduler.schedule(5 seconds, 5 seconds) {
-        channel ! msg
-      }
+      // recover source state using replayed info from channel
+
+      // simulate new project arrival every 5 secs
+      subscription = Some(context.system.scheduler.schedule(5 seconds, 5 seconds) {
+        val project = Project(
+          FreelancerId(UUID.randomUUID.toString),
+          null,
+          Unread,
+          None,
+          None,
+          None,
+          None,
+          None,
+          None,
+          None,
+          None,
+          None
+        )
+        channel.tell(Channel.PublishProject(project), self)
+      })
   }
 
   override def postStop(): Unit = {
-    subscription match {
-      case null =>
-      case sub =>
-        sub.cancel
-    }
+    for (s <- subscription) s.cancel()
     super.postStop
   }
 
