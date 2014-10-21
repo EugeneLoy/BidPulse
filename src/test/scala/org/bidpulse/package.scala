cef862556826b75a8010c066390fc413b588a042
Eugeny Loy <eugeny.loy@gmail.com>
2014-10-21 10:41:30 +0300

    Small cleanup, test improvements

--- src/test/scala/org/bidpulse/package.scala
+++ src/test/scala/org/bidpulse/package.scala
@@ -2,22 +2,24 @@ package org
 
 import java.util.UUID.randomUUID
 
-import akka.actor.{Actor, Props}
+import akka.actor._
 import akka.testkit.{TestKit, ImplicitSender}
 import org.bidpulse.domain.{Project, Unread, FreelancerId}
-import akka.actor.{Actor, Props}
 import akka.testkit._
+import org.scalatest.{Suite, BeforeAndAfterAll}
 import scala.concurrent.duration._
 
 package object bidpulse {
 
   def generateProject = Project(FreelancerId(randomUUID.toString), "", "", null, Unread, None, None, None, None, None, None, None, None, None)
 
-  trait ActorTestingUtils extends ImplicitSender {
+  abstract class  ActorTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with Suite with BeforeAndAfterAll {
 
-    this: TestKit =>
+    override def afterAll {
+      TestKit.shutdownActorSystem(system)
+    }
 
-    def dummy = Props(new Actor { def receive = { case _ => } })
+    def dummy = Props(new Actor { def receive = { case _: Any => } })
     def resendToTestActor = Props(new Actor { def receive = { case msg => testActor ! msg } })
 
     def commonTimeout = (1 seconds).dilated
